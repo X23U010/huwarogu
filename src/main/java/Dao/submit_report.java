422ac9e206563780d84bda312c065b0aa29eaecb
x23u012 <x23u012@PC505N032.chiba-fjb.ac.jp>
2025-11-21 12:35:29 +0900

    Merge branch 'master' of https://github.com/X23U010/huwarogu.git

--- src/main/java/Dao/submit_report.java
+++ src/main/java/Dao/submit_report.java
@@ -8,18 +8,18 @@ import Model.Report;
 public class submit_report extends Base_Dao {
 
 	public boolean Submit_Report(Report report) {
-		
+
 		boolean issubmit = false;
-		
+
 		try {
-			
+
 			this.connect();
-			
-			String sql ="INSERT INTO( report_table{report_id, report_subject_type_id, report_member_id, report_deadline, report_implement, report_location, report_starttime, report_finishtime, report_txt,)VALUES"
-				+ "(?,?,?,?,?,?,?,?,?)";
-			
+
+			String sql = "INSERT INTO( report_table{report_id, report_subject_type_id, report_member_id, report_deadline, report_implement, report_location, report_starttime, report_finishtime, report_txt,)VALUES"
+					+ "(?,?,?,?,?,?,?,?,?)";
+
 			PreparedStatement ps = con.prepareStatement(sql);
-			
+
 			ps.setString(1, report.getReport_id());
 			ps.setString(2, report.getReport_subject_type_id());
 			ps.setString(3, report.getReport_menber_id());
@@ -29,26 +29,26 @@ public class submit_report extends Base_Dao {
 			ps.setString(7, report.getReport_starttime());
 			ps.setString(8, report.getReport_finishtime());
 			ps.setString(9, report.getReport_txt());
-			
-			int record = ps.executeUpdate()	;
-			
-			if(record == 1) {
+
+			int record = ps.executeUpdate();
+
+			if (record == 1) {
 				issubmit = true;
 			}
-			
-			}catch(Exception e){
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				this.disConnect();
+			} catch (SQLException e) {
 				e.printStackTrace();
-				
-			}finally {
-				try {
-					this.disConnect();
-				}catch(SQLException e) {
-					e.printStackTrace();
-				}
 			}
-		
+		}
+
 		return issubmit;
-		
+
 	}
 
 }
