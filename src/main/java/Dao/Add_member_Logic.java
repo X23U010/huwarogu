33fe7bf34570120431ec5fd97d03767108d4f087
x23u046 <x23u046@PC505N025.chiba-fjb.ac.jp>
2025-11-21 11:16:27 +0900

    山田

--- src/main/java/Dao/Add_member_Logic.java
+++ src/main/java/Dao/Add_member_Logic.java
@@ -1,5 +1,12 @@
 package Dao;
 
+import Model.Menber;
+
 public class Add_member_Logic {
+	public boolean execute(Menber member) {
+		
+		Add_member_Dao add_member_Dao = new Add_member_Dao();
+		add_member_Dao.addMember(member);
+	}
 
 }
